f3f744e179b7f89bff5deb5b2da5d632aa7137db
Ariel Cerfoglia <ariel.cerfoglia@gmail.com>
2022-04-15 15:16:19 -0300

    refactor

--- tests/mock_interface/uavcan.equipment.power.BatteryInfo.cpp
+++ tests/mock_interface/uavcan.equipment.power.BatteryInfo.cpp
@@ -4,6 +4,6 @@
 uint32_t uavcan_equipment_power_BatteryInfo_encode(struct uavcan_equipment_power_BatteryInfo* msg, uint8_t* buffer) {
     mock().actualCall("uavcan_equipment_power_BatteryInfo_encode")
           .withParameter("msg", msg)
-          .withParameter("buffer", buffer);
+          .withOutputParameter("buffer", buffer);
     return mock().unsignedIntReturnValue();  
 }
--- tests/tests_DroneCAN_publish_messages.cpp
+++ tests/tests_DroneCAN_publish_messages.cpp
@@ -1,26 +1,26 @@
 #include "common_to_all_tests.h"
 #include <uavcan.equipment.power.BatteryInfo.h>
 
+static DroneCAN_service get_droneCAN_instance_omiting_mock_calls()
+{
+    mock().disable();
+    DroneCAN_service droneCAN_service;
+    mock().enable();
+    return droneCAN_service;
+}
+
 TEST_GROUP(DroneCAN_service_publish_batteryInfo)
 {
+    DroneCAN_service droneCAN_service = get_droneCAN_instance_omiting_mock_calls();
+    
     void teardown()
     {
         teardown_mocks();
     }
 };
 
-static DroneCAN_service get_droneCAN_instance_omiting_mock_calls()
-{
-    mock().disable();
-    DroneCAN_service service;
-    mock().enable();
-    return service;
-}
-
 TEST(DroneCAN_service_publish_batteryInfo, canardBroadcast_is_called_with_right_ID_and_signature)
 {
-    DroneCAN_service service = get_droneCAN_instance_omiting_mock_calls();
-
     mock().expectOneCall("canardBroadcast")
           .withParameter("data_type_signature", UAVCAN_EQUIPMENT_POWER_BATTERYINFO_SIGNATURE)
           .withParameter("data_type_id", UAVCAN_EQUIPMENT_POWER_BATTERYINFO_ID)
@@ -28,38 +28,46 @@ TEST(DroneCAN_service_publish_batteryInfo, canardBroadcast_is_called_with_right_
     mock().ignoreOtherCalls();
 
     uavcan_equipment_power_BatteryInfo battery_message;
-    service.publish_message(battery_message);
+    droneCAN_service.publish_message(battery_message);
 }
 
 TEST(DroneCAN_service_publish_batteryInfo, canardBroadcast_is_called_with_right_priority)
 {
-    DroneCAN_service service = get_droneCAN_instance_omiting_mock_calls();
-
     mock().expectOneCall("canardBroadcast")
           .withParameter("priority", CANARD_TRANSFER_PRIORITY_LOW)
           .ignoreOtherParameters();
     mock().ignoreOtherCalls();
 
     uavcan_equipment_power_BatteryInfo battery_message;
-    service.publish_message(battery_message);
+    droneCAN_service.publish_message(battery_message);
 }
 
 
 TEST(DroneCAN_service_publish_batteryInfo, encode_function_is_called_with_battery_message)
 {
-    DroneCAN_service service = get_droneCAN_instance_omiting_mock_calls();
-    
     uavcan_equipment_power_BatteryInfo battery_message;
     mock().expectOneCall("uavcan_equipment_power_BatteryInfo_encode")
           .withParameter("msg", (void*)&battery_message)
           .ignoreOtherParameters();          
     mock().ignoreOtherCalls();
 
-    service.publish_message(battery_message);
+    droneCAN_service.publish_message(battery_message);
 }
 
 //- when batteryInfo is published, the canardBroadcast is called with the message and message_length from encodeMessage
-TEST(DroneCAN_service_publish_batteryInfo, canardBroascast_is_called_with_encoded_message)
-{
-    FAIL("Continue from here. Next step: tests for CAN bus write sequence.");
-}
+// TEST(DroneCAN_service_publish_batteryInfo, canardBroascast_is_called_with_encoded_message)
+// {
+//     uavcan_equipment_power_BatteryInfo battery_message;
+//     const uint32_t ENCODED_MESSAGE_LENGTH = 3; 
+//     uint8_t message_buffer[] = {1, 2, 3};
+//     mock().expectOneCall("uavcan_equipment_power_BatteryInfo_encode")
+//           .withOutputParameterReturning("buffer", &message_buffer, sizeof(message_buffer))
+//           .ignoreOtherParameters()
+//           .andReturnValue(ENCODED_MESSAGE_LENGTH);
+    
+//     mock().expectOneCall("canardBroadcast")
+//           .withParameter("payload", message_buffer)
+//           .withParameter("payload_len", ENCODED_MESSAGE_LENGTH);
+
+//     droneCAN_service.publish_message(battery_message);
+// }
